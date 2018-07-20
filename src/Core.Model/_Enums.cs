c27efabb9c36692692f5a03e61d600802692a8e8
Gladiator <Gladiator@DESKTOP-D5Q8Q8S>
2018-07-20 09:46:46 +0430

    Enums Added

--- src/Core.Model/_Enums.cs
+++ src/Core.Model/_Enums.cs
@@ -2,5 +2,23 @@
 
 namespace Core.Model
 {
+    public enum DocState
+    {
+        Unkown = 0
+    }
 
+    public enum AttachmentType
+    {
+        Unkown = 0
+    }
+
+    public enum SendType
+    {
+        Unkown = 0
+    }
+
+    public enum DocumentType
+    {
+        Unkown = 0
+    }
 }
